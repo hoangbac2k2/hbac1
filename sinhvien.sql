1823609208beb23072b5cd7593fdf57c0851564e
hoangbac2k2 <89562248+hoangbac2k2@users.noreply.github.com>
2023-08-16 13:37:23 +0700

    Add files via upload

---
+++ sinhvien.sql
@@ -0,0 +1,3 @@
+﻿insert into diem values ('1','hoàng bắc','vp','08-18-2002','5');
+insert into diem values ('2','hoàng bắc','vp','08-18-2002','5');
+insert into diem values ('3','hoàng bắc','vp','08-18-2002','5');
